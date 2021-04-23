10b765cfc10bd19c4b146795b5f65adf86343f90
zuoyongqiang <zuoyongqiang@tal.com>
2021-04-23 23:30:56 +0800

    增加单个发送消息功能

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"flag"
 	"os"
+	"strings"
 
 	"github.com/apache/rocketmq-client-go/v2"
 	"github.com/apache/rocketmq-client-go/v2/primitive"
@@ -12,25 +13,20 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/rlog"
 )
 
-var namesrv = flag.String("n", "127.0.0.1:9876", "rocketmq namesrv for example 127.0.0.1:9876")
-var topic = flag.String("t", "test-topic", "rocketmq topic")
-var filePath = flag.String("f", "", "rocketmq message.txt for example /home/work/message.txt")
+var namesrv = flag.String("n", "127.0.0.1:9876", "rocketmq namesrv for example: -n 127.0.0.1:9876")
+var topic = flag.String("t", "test-topic", "rocketmq topic for example: -t test-topic")
+var filePath = flag.String("f", "", "rocketmq message.txt for example: -f /home/work/message.txt")
+var message = flag.String("m", "", "rocketmq single message for example: -m \"{\\\"userId\\\":123}\"")
 
 // Package main implements a simple producer to send message.txt.
 func main() {
 	flag.Parse()
-	rlog.Info("read command content:", map[string]interface{}{"namesrv": *namesrv, "topic": *topic, "messageFile": *filePath})
+	rlog.Info("read command content:", map[string]interface{}{"namesrv": *namesrv, "topic": *topic, "messageFile": *filePath, "message": *message})
 
-	if "" == *filePath {
-		rlog.Error("message file of path is empty!!!", nil)
+	if "" == *filePath && *message == "" {
+		rlog.Error("message or message file is not exist !!!", nil)
 		os.Exit(1)
 	}
-	msgFile, e := os.OpenFile(*filePath, os.O_RDONLY, 0644)
-	if e != nil {
-		rlog.Error("open file error", map[string]interface{}{"": e.Error()})
-		os.Exit(1)
-	}
-	defer msgFile.Close()
 
 	p, _ := rocketmq.NewProducer(
 		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{*namesrv})),
@@ -42,15 +38,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	reader := bufio.NewScanner(msgFile)
-	for reader.Scan() {
-		line := reader.Text()
-		sendMessage(p, *topic, line)
-	}
-
-	err = msgFile.Close()
-	if err != nil {
-		rlog.Error("close file failed", map[string]interface{}{"error": err.Error()})
+	if "" != *filePath {
+		processMessageFile(p, *topic, *filePath)
+	} else if "" != *message {
+		processMessage(p, *topic, *message)
 	}
 
 	err = p.Shutdown()
@@ -59,6 +50,34 @@ func main() {
 	}
 
 }
+
+func processMessage(p rocketmq.Producer, topic string, message string) {
+	sendMessage(p, topic, message)
+}
+
+func processMessageFile(p rocketmq.Producer, topic string, fileName string) {
+	msgFile, e := os.OpenFile(fileName, os.O_RDONLY, 0644)
+	if e != nil {
+		rlog.Error("open file error", map[string]interface{}{"": e.Error()})
+		os.Exit(1)
+	}
+	defer msgFile.Close()
+
+	reader := bufio.NewScanner(msgFile)
+	for reader.Scan() {
+		line := strings.TrimSpace(reader.Text())
+		if "" == line {
+			continue
+		}
+		sendMessage(p, topic, line)
+	}
+
+	err := msgFile.Close()
+	if err != nil {
+		rlog.Error("close file failed", map[string]interface{}{"error": err.Error()})
+	}
+}
+
 func sendMessage(p rocketmq.Producer, topic string, body string) {
 	msg := &primitive.Message{
 		Topic: topic,
